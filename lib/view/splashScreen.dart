bc160b57fbfeec61948907ddec9106c154cba472
Kanishka Anand <kanishkaadz@gmail.com>
2024-07-26 21:49:26 +0530

    Update splashScreen.dart

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -8,8 +8,10 @@ class splashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.network("https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-4.1.0&q=45&auto=format&w=926&fit=clip"),
+          SizedBox(height: 30,),
           Text("SED LYF"),
           Text("A Meme App !!"),
         ],
