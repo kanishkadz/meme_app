baba0383b77b46568638f0205616477606db388f
Kanishka Anand <kanishkaadz@gmail.com>
2024-07-29 22:04:02 +0530

    Update fetchMeme.dart

--- lib/controller/fetchMeme.dart
+++ lib/controller/fetchMeme.dart
@@ -4,7 +4,7 @@ class FetchMeme{
   fetchNewMeme()async{
 
     Response response = await get(Uri.parse("https://api.imgflip.com/get_memes"));
-    print(response);
+    print(response.body);
 
   }
 }
